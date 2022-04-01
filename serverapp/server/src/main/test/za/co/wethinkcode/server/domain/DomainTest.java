bb54d8c7bbbeed7cd0da180a6a2de629937f4144
soulo <tsoulo@student.wethinkcode.co.za>
2022-03-31 18:03:52 -0700

    updates

---
+++ serverapp/server/src/main/test/za/co/wethinkcode/server/domain/DomainTest.java
@@ -0,0 +1,28 @@
+package za.co.wethinkcode.server.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DomainTest {
+
+    @Test
+    void createCommute() {
+    }
+
+    @Test
+    void stopCommute() {
+    }
+
+    @Test
+    void calculateGpsDistanceAndAmount() {
+    }
+
+    @Test
+    void calculateStationAmount() {
+    }
+
+    @Test
+    void checkIfUserExits() {
+    }
+}
---
+++ serverapp/server/src/main/test/za/co/wethinkcode/server/domain/GoWalletTest.java
@@ -0,0 +1,12 @@
+package za.co.wethinkcode.server.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoWalletTest {
+
+    @Test
+    void getBalance() {
+    }
+}
